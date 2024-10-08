8e3180ffaf927cf212037fe6f24f9d25f383069c
RICHER5678 <bagsricher@gmail.com>
2024-10-08 15:38:29 +0300

    Time out notifcations, contact import logic,

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel Vue App</title>
+        <title>vue-laravel-app</title>
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="antialiased" data-bs-theme="dark">
