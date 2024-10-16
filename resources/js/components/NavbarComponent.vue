38ae995ab52d52b41beb87711ca33c911a0910f7
RICHER5678 <bagsricher@gmail.com>
2024-10-16 18:42:10 +0300

    READ ME

--- resources/js/components/NavbarComponent.vue
+++ resources/js/components/NavbarComponent.vue
@@ -4,14 +4,15 @@
       <b>
         <img src="/public/fav.jpg" class="mb-1" alt="Bootstrap" width="30" height="24">
         <a class="navbar-brand" href="#"> SMS</a></b>
-    
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li><router-link class="nav-link" to="/">Dashboard</router-link></li>
-          
+
           <!-- <li><a class="nav-link d-flex align-items-center"   href="#"><i class="fa fa-home p-1"></i> Dashboard</a></li> -->
 
           <li class="nav-item dropdown">
@@ -20,8 +21,8 @@
             </a>
             <ul class="dropdown-menu">
               <li><a class="dropdown-item" href="#"><i class="fas fa-sms"></i> Quick SMS</a></li>
-              <li><a class="dropdown-item" href="#"><i class="fas fa-file-alt"></i> Templates</a></li>
-              <li><router-link class="nav-link" to="/templates"><i class="fa fa-file-alt me-1"></i>Templates</router-link></li>
+              <li><router-link class="nav-link" to="/templates"><i
+                    class="fa fa-file-alt me-1"></i>Templates</router-link></li>
             </ul>
           </li>
           <li class="nav-item">
@@ -33,9 +34,11 @@
               Company Management
             </a>
             <ul class="dropdown-menu">
-              <li><router-link class="nav-link" to="/organisations"><i class="fa fa-sitemap me-1 "></i>Organisations</router-link></li>
-              <li><router-link class="nav-link" to="/departments"><i class="fa fa-layer-group me-1"></i>Departments</router-link></li>
-            
+              <li><router-link class="nav-link" to="/organisations"><i
+                    class="fa fa-sitemap me-1 "></i>Organisations</router-link></li>
+              <li><router-link class="nav-link" to="/departments"><i
+                    class="fa fa-layer-group me-1"></i>Departments</router-link></li>
+
               <li><a class="dropdown-item" href="#"><i class="fas fa-user-tag"></i> Roles</a></li>
             </ul>
           </li>
@@ -44,7 +47,8 @@
               User Management
             </a>
             <ul class="dropdown-menu">
-              <li><router-link class="nav-link" to="/groups"><i class="fa fa-users-cog me-1"></i>Groups</router-link></li>
+              <li><router-link class="nav-link" to="/groups"><i class="fa fa-users-cog me-1"></i>Groups</router-link>
+              </li>
               <li><a class="dropdown-item" href="#"><i class="fas fa-user"></i> Users</a></li>
             </ul>
           </li>
@@ -59,7 +63,7 @@
           </li>
           <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-               Reports
+              Reports
             </a>
             <ul class="dropdown-menu">
               <li><a class="dropdown-item" href="#"><i class="fas fa-briefcase"></i> Contact Reports</a></li>
@@ -68,7 +72,7 @@
           </li>
           <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-             Settings
+              Settings
             </a>
             <ul class="dropdown-menu">
               <li><a class="dropdown-item" href="#"><i class="fas fa-id-badge"></i> Sender ID</a></li>
@@ -76,22 +80,26 @@
             </ul>
           </li>
         </ul>
-       
+
         <!-- account button -->
-<div style="margin-right: auto;" class="btn-group ">
-  <button type="button" class="btn btn-success"><i class="fa fa-user p-2"></i>Account</button>
-  <button type="button" class="btn btn-success dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" aria-expanded="false">
-    <span class="visually-hidden">Toggle Dropdown</span>
-  </button>
-  <ul class="dropdown-menu">
-    <li><a class="dropdown-item" href="#">My Profile</a></li>
-    <!-- <li><a class="dropdown-item" href="#">Another action</a></li>
+        <div style="margin-right: auto;" class="btn-group ">
+          <button type="button" class="btn btn-success"><i class="fa fa-user p-2"></i>Account</button>
+          <button type="button" class="btn btn-success dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown"
+            aria-expanded="false">
+            <span class="visually-hidden">Toggle Dropdown</span>
+          </button>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">My Profile</a></li>
+            <!-- <li><a class="dropdown-item" href="#">Another action</a></li>
     <li><a class="dropdown-item" href="#">Something else here</a></li> -->
-    <li><hr class="dropdown-divider"></li>
-    <li> <a class="dropdown-item" href="#"> Logout<i class="fa fa-power-off p-2" aria-hidden="true"></i></a></li>
-  </ul>
-</div>
-     
+            <li>
+              <hr class="dropdown-divider">
+            </li>
+            <li> <a class="dropdown-item" href="#"> Logout<i class="fa fa-power-off p-2" aria-hidden="true"></i></a>
+            </li>
+          </ul>
+        </div>
+
       </div>
     </div>
   </nav>
