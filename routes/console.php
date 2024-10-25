b8162c30ea306469b47fb7e6807300b11a789338
RICHER5678 <bagsricher@gmail.com>
2024-10-25 13:00:36 +0300

    configuring (php artisan sms) for directly starting the project, re-designing the contacts form

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,24 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Http;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+
+
+Artisan::command('sms', function () {
+  // open URL
+    exec('xdg-open http://127.0.0.1:8000'); // Linux
+
+    // Fetch the user's IP address
+    $ip = request()->ip();
+
+    if (!$ip) {
+        $this->error('IP address could not be determined in CLI mode.');
+    } else {
+        $this->info("Tracking IP Address: $ip");
+    }
+})->purpose('Open SMS PROJECT and start tracking IP address.');
